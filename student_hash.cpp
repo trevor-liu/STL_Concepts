04fbac33a2fd62d8e9cb9ae4a818c4c9ef2dab13
Trevor Liu <trevorcliu@hotmail.com>
2020-03-04 03:24:01 -0700

    Initial commit

---
+++ student_hash.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+struct StudentRecord {
+	string name;
+	int id, grade;
+};
+
+int main() {
+	// this doesn't work!
+	// unordered_set <StudentRecord> table;
+
+	// do something different
+	
+	return 0;
+}
